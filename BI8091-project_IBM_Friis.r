6f20295c34ed720491e06995517b570468398bcb
mefriis <mikkel.l.friis@gmail.com>
2024-11-26 20:43:30 +0100

    Added winter_update function

--- BI8091-project_IBM_Friis.r
+++ BI8091-project_IBM_Friis.r
@@ -42,16 +42,23 @@ hgd_view()
 num_fish <- 1000
 #### Simulation Runtime, years
 max_time <- 10
-#### Juvenile Mortality, factor
-juv_mort <- 0.3
+#### Juvenile Vinter Mortality, factor
+juv_mort <- 0.5
+#### Migrants Vinter Mortality, factor
+mig_mort <- 0.1
 #### Base Mortality for Migrations, factor
 mig_mort_base <- 0.2
 #### Base Mortality at Sea, factor
 sea_mort_base <- 0.2
 #### Growth of Juveniles, length in cm
-growth_juv <- sample(2:7)
+growth_juv <- function() {
+    return(sample(2:7, 1))
+}
 #### Growth of Migrants, length in cm
-growth_mig <- sample(5:15)
+growth_mig <- function() {
+    return(sample(5:15, 1))
+}
+
 #### Flow Threshold for Ice Hatch Migrations, flow-rate
 flow_th <- 10
 #### Turbine Mortatily, factor
@@ -71,8 +78,24 @@ initialize_population <- function(n) {
     )
 }
 
+winter_update <- function(population) {
+    juveniles <- population[population$stage == "juvenile" & population$alive, ]
+    migrants <- population[population$stage == "migrant" & population$alive, ]
+
+    juveniles$alive <- runif(nrow(juveniles)) > juv_mort
+    migrants$alive <- runif(nrow(migrants)) > mig_mort_base
+
+    juveniles$length <- juveniles$length + growth_juv()
+
+    population <- rbind(juveniles, migrants)
+    return(population)
+}
+
 
 
 population <- initialize_population(100)
+population2 <- winter_update(population)
+
 print(population)
 hist(population$length)
+hist(population2$length)
