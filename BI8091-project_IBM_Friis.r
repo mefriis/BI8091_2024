079b61b7431959373e3a3bcfeac0e42ef3120678
mefriis <mikkel.l.friis@gmail.com>
2024-11-26 18:26:44 +0100

    Added initialize_population function

--- BI8091-project_IBM_Friis.r
+++ BI8091-project_IBM_Friis.r
@@ -49,12 +49,30 @@ mig_mort_base <- 0.2
 #### Base Mortality at Sea, factor
 sea_mort_base <- 0.2
 #### Growth of Juveniles, length in cm
-growth_juv <- 5
-#### Growth of Residents, length in cm
-growth_res <- 2
+growth_juv <- sample(2:7)
 #### Growth of Migrants, length in cm
-growth_mig <- 10
+growth_mig <- sample(5:15)
 #### Flow Threshold for Ice Hatch Migrations, flow-rate
 flow_th <- 10
 #### Turbine Mortatily, factor
 turb_mort <- 0.5
+
+initialize_population <- function(n) {
+    stages <- sample(c("juvenile", "migrant"), n, replace = TRUE, prob = c(0.8, 0.2))
+    lengths <- ifelse(
+        stages == "juvenile", pmax(rnorm(n, mean = 12.5, sd = 5), 5), rnorm(n, mean = 62.5, sd = 15)
+    )
+
+    data.frame(
+        id = 1:n,
+        stage = stages,
+        length = lengths,
+        alive = TRUE
+    )
+}
+
+
+
+population <- initialize_population(100)
+print(population)
+hist(population$length)
